4893259d3bc99350729b534a8892d1d574ce90e7
Aleksey Kurkov <aleksey.kurkov@gmail.com>
2016-03-26 19:32:14 +0200

    Initial commit

---
+++ app/src/main/java/com/kurkov/aleksey/androidemailapp/Config.java
@@ -0,0 +1,8 @@
+package com.kurkov.aleksey.androidemailapp;
+
+/* Created by Aleksey Kurkov on 26.03.2016. */
+
+public class Config {
+    public static final String EMAIL ="your-gmail-username";
+    public static final String PASSWORD ="your-gmail-password";
+}
